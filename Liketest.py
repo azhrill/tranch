78f133c92ce549da64821b18999217f8876dfeb9
TreeBot <33048684+Treerach@users.noreply.github.com>
2017-10-31 08:56:26 +0700

    Update Liketest.py

--- Liketest.py
+++ Liketest.py
@@ -18,6 +18,73 @@ sys.setdefaultencoding('utf-8')
 i = 0
 c_text = """like"""
 
+KAC = [cl,ki,kk,ks]
+mid = cl.getProfile().mid
+Amid = ki.getProfile().mid
+kimid = kk.getProfile().mid
+ki2mid = ks.getProfile().mid
+Bots = [mid,Amid,kimid,ki2mid]
+admin = ["u9489706a45fcf78bea076c6b77f7067d","ucd886b532f581aa4de98af5898719392"]
+me = cl.getProfile().mid
+bot1 = cl.getProfile().mid
+main = cl.getProfile().mid
+kicker1 = ki.getProfile().mid
+bots = me + kicker1
+protectname = []
+protecturl = []
+protection = []
+autocancel = {}
+autoinvite = []
+autoleaveroom = []
+
+wait = {
+    'contact':False,
+    'autoJoin':True,
+    'autoCancel':{"on":True,"members":1},
+    'leaveRoom':True,
+    'timeline':False,
+    'autoAdd':False,
+    'message':"Thanks for add me",
+    "lang":"JP",
+    "comment":"Thanks for add me",
+    "commentOn":False,
+    "commentBlack":{},
+    "wblack":False,
+    "dblack":False,
+    "clock":False,
+    "blacklist":{},
+    "wblacklist":False,
+    "dblacklist":False,
+	"pnharfbot":{},
+    "pname":{},
+    "pro_name":{},
+	"posts":True,
+	}
+	
+wait2 = {
+	'readMember':{},
+	'readPoint':{},
+	'ROM':{},
+	'setTime':{}
+    }
+	
+setTime = {}
+setTime = wait2["setTime"]
+
+res = {
+    'num':{},
+    'us':{},
+    'au':{},
+}
+
+
+def Cmd(string, commands): #"¤","¤¤","¤¤¤","¤¤¤","¤¤","¤"
+    tex = [""]
+    for texX in tex:
+        for command in commands:
+            if string ==texX + command:
+                return True
+    return False
 
 def sendMessage(to, text, contentMetadata={}, contentType=0):
     mes = Message()
@@ -39,3 +106,29 @@ def autolike(op):
 					i += 1
     except Exception as e:
             print e
+def bot(op):
+    try:
+        if op.type == 0:
+            return
+        if op.type == 5:
+            if wait["autoAdd"] == True:
+                cl.findAndAddContactsByMid(op.param1)
+                if (wait["message"] in [""," ","\n",None]):
+                    pass
+                else:
+                    cl.sendText(op.param1,str(wait["message"]))						
+        if op.type == 13:
+            if mid in op.param3:
+                G = cl.getGroup(op.param1)
+                if wait["autoJoin"] == True:
+                    if wait["autoCancel"]["on"] == True:
+                        if len(G.members) <= wait["autoCancel"]["members"]:
+                            cl.rejectGroupInvitation(op.param1)
+                        else:
+                            cl.acceptGroupInvitation(op.param1)
+                    else:
+                        cl.acceptGroupInvitation(op.param1)
+                elif wait["autoCancel"]["on"] == True:
+                    if len(G.members) <= wait["autoCancel"]["members"]:
+                        cl.rejectGroupInvitation(op.param1)		
+		    else:	
